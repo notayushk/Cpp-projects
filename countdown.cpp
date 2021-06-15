38cda213ef6e236b2f5ddd42d1daf1921ce929a1
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-06-15 20:54:55 +0530

    Update countdown.cpp

--- countdown.cpp
+++ countdown.cpp
@@ -1,3 +1,27 @@
+/* Problem:-
+You need to make a countdown app.
+Given a number N as input, output numbers from N to 1 on separate lines. 
+Also, when the current countdown number is a multiple of 5, the app should output "Beep".
+
+Sample Input:
+12
+
+Sample Output: 
+12
+11
+10
+Beep
+9
+8
+7
+6
+5
+Beep
+4
+3
+2
+1
+*/
 #include <iostream>
 using namespace std;
 
