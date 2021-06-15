e664f10d712f0a4f17fb6942f72271b7d3c730f2
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-06-15 20:50:31 +0530

    Create countdown.cpp

---
+++ countdown.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+    while ( number > 0) {
+        cout << number << endl;
+        if (number % 5 == 0) {
+            cout << "Beep" << endl;
+        }
+        n--;
+    }
+    
+    return 0;
+}
