474fda32db2fd293458ed5a441cf8c5133056d34
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-06-15 20:47:48 +0530

    Create transportation.cpp

---
+++ transportation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //your code goes here
+    int passengers;
+    cin >> passengers;
+    cout << 50 - passengers % 50;
+    
+    return 0;
+}
